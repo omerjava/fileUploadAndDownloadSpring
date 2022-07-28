3036af3c22a6836a55ad97a18c76274a2e334260
omerjava <ofk45@hotmail.com>
2022-07-29 01:03:04 +0200

    initial

---
+++ src/test/java/com/fileUploadAndDownloadSpring/FileUploadAndDownloadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileUploadAndDownloadSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadAndDownloadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
